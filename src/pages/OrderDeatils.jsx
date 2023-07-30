1b001c882a17654faecd3f646f1cc328b58f20b9
Anushtha Pandit <kumarsen.bash@gmall.com>
2023-07-30 15:35:32 +0530

    detils page order status fix

--- src/pages/OrderDeatils.jsx
+++ src/pages/OrderDeatils.jsx
@@ -52,12 +52,19 @@ const OrderDeatils = () => {
 								class="absolute inset-0 bg-orange-200 opacity-50 rounded-full"></span>
 							<span class="relative text-xs">Pending</span>
 						</span>
-					) : (
+					) : details.status === "placed" ? (
 						<span class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
 							<span
 								aria-hidden
 								class="absolute inset-0 bg-green-200 opacity-50 rounded-full"></span>
 							<span class="relative text-xs">placed</span>
+						</span>
+					) : (
+						<span class="relative inline-block px-3 py-1 font-semibold text-orange-900 leading-tight">
+							<span
+								aria-hidden
+								class="absolute inset-0 bg-orange-200 opacity-50 rounded-full"></span>
+							<span class="relative text-xs">Cancelled</span>
 						</span>
 					)}
 				</p>
