06eb71d49c3647af8a53ac8d5cfe8055e63c4aa1
Anushtha Pandit <kumarsen.bash@gmall.com>
2023-07-30 14:42:20 +0530

    order details page

--- src/pages/OrderDeatils.jsx
+++ src/pages/OrderDeatils.jsx
@@ -1,14 +1,13 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 import { fetch_single_order } from "../schemas/orders.schema";
 import Loader from "../components/Loader";
 
 const OrderDeatils = () => {
-
-	const { id: order_doc_id } = useParams()
+	const { id: order_doc_id } = useParams();
 	const [isLoading, setisLoading] = useState(false);
-	const [details, setdetails] = useState({})
+	const [details, setdetails] = useState({});
 
 	useEffect(() => {
 		(async () => {
@@ -16,9 +15,7 @@ const OrderDeatils = () => {
 				setisLoading(true);
 
 				const data = await fetch_single_order(order_doc_id);
-				
 				setdetails(data);
-
 			} catch (error) {
 			} finally {
 				setisLoading(false);
@@ -34,6 +31,10 @@ const OrderDeatils = () => {
 		);
 	}
 
+	if (!details.status) {
+		return <center> NO Page Found!</center>;
+	}
+
 	return (
 		<div class="py-14 px-4 md:px-6 2xl:px-20 2xl:container 2xl:mx-auto">
 			<div class="flex justify-start item-start space-y-2 flex-col">
@@ -41,7 +42,24 @@ const OrderDeatils = () => {
 					Order #{details.doc_id}
 				</h1>
 				<p class="text-base font-medium leading-6">
-					{details.created_at?.toDate()?.utc}
+					{details.created_at?.toDate()?.toString()}
+				</p>
+				<p class="whitespace-no-wrap text-blue-900 border-gray-500 text-sm leading-5">
+					{details.status === "pending" ? (
+						<span class="relative inline-block px-3 py-1 font-semibold text-orange-900 leading-tight">
+							<span
+								aria-hidden
+								class="absolute inset-0 bg-orange-200 opacity-50 rounded-full"></span>
+							<span class="relative text-xs">Pending</span>
+						</span>
+					) : (
+						<span class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+							<span
+								aria-hidden
+								class="absolute inset-0 bg-green-200 opacity-50 rounded-full"></span>
+							<span class="relative text-xs">placed</span>
+						</span>
+					)}
 				</p>
 			</div>
 			<div class="mt-10 flex flex-col xl:flex-row jusitfy-center items-stretch w-full xl:space-x-8 space-y-4 md:space-y-6 xl:space-y-0">
@@ -50,88 +68,48 @@ const OrderDeatils = () => {
 						<p class="text-lg md:text-xl dark:text-white font-semibold leading-6 xl:leading-5 text-gray-800">
 							Items
 						</p>
-						<div class="mt-4 md:mt-6 flex flex-col md:flex-row justify-start items-start md:items-center md:space-x-6 xl:space-x-8 w-full">
-							<div class="pb-4 md:pb-8 w-full md:w-40">
-								<img
-									class="w-full hidden md:block"
-									src="/imgs/aloo-chop.png"
-									alt="dress"
-								/>
-								<img
-									class="w-full md:hidden"
-									src="/imgs/aloo-chop.png"
-									alt="dress"
-								/>
-							</div>
-							<div class="border-b border-gray-200 md:flex-row flex-col flex justify-between items-start w-full pb-8 space-y-4 md:space-y-0">
-								<div class="w-full flex flex-col justify-start items-start space-y-8">
-									<h3 class="text-xl dark:text-white xl:text-2xl font-semibold leading-6 text-gray-800">
-										Aloo Chop
-									</h3>
-									<div class="flex justify-start items-start flex-col space-y-2">
-										<p class="text-sm dark:text-white leading-none text-gray-800">
-											<span class="dark:text-gray-400 text-gray-300">
-												Restaurant:{" "}
-											</span>{" "}
-											A Very Fancy Restaurant
+						{details.cartItems.map((v, i) => (
+							<div
+								key={i}
+								class="mt-4 md:mt-6 flex flex-col md:flex-row justify-start items-start md:items-center md:space-x-6 xl:space-x-8 w-full">
+								<div class="pb-4 md:pb-8 w-full md:w-40">
+									<img
+										class="w-full hidden md:block"
+										src={v.imageURL}
+										alt="food"
+									/>
+									<img class="w-full md:hidden" src={v.imageURL} alt="food" />
+								</div>
+
+								<div class="border-b border-gray-200 md:flex-row flex-col flex justify-between items-start w-full pb-8 space-y-4 md:space-y-0">
+									<div class="w-full flex flex-col justify-start items-start space-y-8">
+										<h3 class="text-xl dark:text-white xl:text-2xl font-semibold leading-6 text-gray-800">
+											{v.title}
+										</h3>
+										{/* <div class="flex justify-start items-start flex-col space-y-2">
+											<p class="text-sm dark:text-white leading-none text-gray-800">
+												<span class="dark:text-gray-400 text-gray-300">
+													Restaurant:{" "}
+												</span>{" "}
+												
+											</p>
+										</div> */}
+									</div>
+									<div class="flex justify-between space-x-8 items-start w-full">
+										<p class="text-base dark:text-white xl:text-lg leading-6">
+											&pound;{v.price}
+											{/* <span class="text-red-300 line-through"> $45.00</span> */}
+										</p>
+										<p class="text-base dark:text-white xl:text-lg leading-6 text-gray-800">
+											{v.qty}
+										</p>
+										<p class="text-base dark:text-white xl:text-lg font-semibold leading-6 text-gray-800">
+											&pound;{v.price * v.qty}
 										</p>
 									</div>
 								</div>
-								<div class="flex justify-between space-x-8 items-start w-full">
-									<p class="text-base dark:text-white xl:text-lg leading-6">
-										$1.00{" "}
-										{/* <span class="text-red-300 line-through"> $45.00</span> */}
-									</p>
-									<p class="text-base dark:text-white xl:text-lg leading-6 text-gray-800">
-										01
-									</p>
-									<p class="text-base dark:text-white xl:text-lg font-semibold leading-6 text-gray-800">
-										$1
-									</p>
-								</div>
 							</div>
-						</div>
-						<div class="mt-6 md:mt-0 flex justify-start flex-col md:flex-row items-start md:items-center space-y-4 md:space-x-6 xl:space-x-8 w-full">
-							<div class="w-full md:w-40">
-								<img
-									class="w-full hidden md:block"
-									src="/imgs/panipuri.png"
-									alt="dress"
-								/>
-								<img
-									class="w-full md:hidden"
-									src="/imgs/panipuri.png"
-									alt="dress"
-								/>
-							</div>
-							<div class="flex justify-between items-start w-full flex-col md:flex-row space-y-4 md:space-y-0">
-								<div class="w-full flex flex-col justify-start items-start space-y-8">
-									<h3 class="text-xl dark:text-white xl:text-2xl font-semibold leading-6 text-gray-800">
-										High Quaility Panipuri
-									</h3>
-									<div class="flex justify-start items-start flex-col space-y-2">
-										<p class="text-sm dark:text-white leading-none text-gray-800">
-											<span class="dark:text-gray-400 text-gray-300">
-												Restaurant:{" "}
-											</span>{" "}
-											A Very Fancy Restaurant
-										</p>
-									</div>
-								</div>
-								<div class="flex justify-between space-x-8 items-start w-full">
-									<p class="text-base dark:text-white xl:text-lg leading-6">
-										$2.00
-										{/* <span class="text-red-300 line-through"> $30.00</span> */}
-									</p>
-									<p class="text-base dark:text-white xl:text-lg leading-6 text-gray-800">
-										01
-									</p>
-									<p class="text-base dark:text-white xl:text-lg font-semibold leading-6 text-gray-800">
-										$2
-									</p>
-								</div>
-							</div>
-						</div>
+						))}
 					</div>
 					<div class="flex justify-center flex-col md:flex-row flex-col items-stretch w-full space-y-4 md:space-y-0 md:space-x-6 xl:space-x-8">
 						<div class="flex flex-col px-4 py-6 md:p-6 xl:p-8 w-full bg-gray-50 dark:bg-gray-800 space-y-6">
@@ -163,7 +141,7 @@ const OrderDeatils = () => {
 										Shipping
 									</p>
 									<p class="text-base dark:text-gray-300 leading-4 text-gray-600">
-									&pound;{10.0}
+										&pound;{10.0}
 									</p>
 								</div>
 							</div>
@@ -172,7 +150,7 @@ const OrderDeatils = () => {
 									Total
 								</p>
 								<p class="text-base dark:text-gray-300 font-semibold leading-4 text-gray-600">
-								&pound;{details.total_price}
+									&pound;{details.total_price}
 								</p>
 							</div>
 						</div>
@@ -216,42 +194,16 @@ const OrderDeatils = () => {
 					<div class="flex flex-col md:flex-row xl:flex-col justify-start items-stretch h-full w-full md:space-x-6 lg:space-x-8 xl:space-x-0">
 						<div class="flex flex-col justify-start items-start flex-shrink-0">
 							<div class="flex justify-center w-full md:justify-start items-center space-x-4 py-8 border-b border-gray-200">
-								<img
-									src="https://i.ibb.co/5TSg7f6/Rectangle-18.png"
-									alt="avatar"
-								/>
 								<div class="flex justify-start items-start flex-col space-y-2">
 									<p class="text-base dark:text-white font-semibold leading-4 text-left text-gray-800">
-										David Kent
-									</p>
-									<p class="text-sm dark:text-gray-300 leading-5 text-gray-600">
-										10 Previous Orders
+										{details.name}
 									</p>
 								</div>
 							</div>
 
 							<div class="flex justify-center text-gray-800 dark:text-white md:justify-start items-center space-x-4 py-4 border-b border-gray-200 w-full">
-								<svg
-									width="24"
-									height="24"
-									viewBox="0 0 24 24"
-									fill="none"
-									xmlns="http://www.w3.org/2000/svg">
-									<path
-										d="M19 5H5C3.89543 5 3 5.89543 3 7V17C3 18.1046 3.89543 19 5 19H19C20.1046 19 21 18.1046 21 17V7C21 5.89543 20.1046 5 19 5Z"
-										stroke="currentColor"
-										stroke-linecap="round"
-										stroke-linejoin="round"
-									/>
-									<path
-										d="M3 7L12 13L21 7"
-										stroke="currentColor"
-										stroke-linecap="round"
-										stroke-linejoin="round"
-									/>
-								</svg>
 								<p class="cursor-pointer text-sm leading-5 ">
-									david89@gmail.com
+									{details.contact}
 								</p>
 							</div>
 						</div>
@@ -259,20 +211,20 @@ const OrderDeatils = () => {
 							<div class="flex justify-center md:justify-start xl:flex-col flex-col md:space-x-6 lg:space-x-8 xl:space-x-0 space-y-4 xl:space-y-12 md:space-y-0 md:flex-row items-center md:items-start">
 								<div class="flex justify-center md:justify-start items-center md:items-start flex-col space-y-4 xl:mt-8">
 									<p class="text-base dark:text-white font-semibold leading-4 text-center md:text-left text-gray-800">
-										Shipping Address
+										Address
 									</p>
 									<p class="w-48 lg:w-full dark:text-gray-300 xl:w-48 text-center md:text-left text-sm leading-5 text-gray-600">
-										180 North King Street, Northhampton MA 1060
+										{details.address}, {details.city} {details.postcode}
 									</p>
 								</div>
-								<div class="flex justify-center md:justify-start items-center md:items-start flex-col space-y-4">
+								{/* <div class="flex justify-center md:justify-start items-center md:items-start flex-col space-y-4">
 									<p class="text-base dark:text-white font-semibold leading-4 text-center md:text-left text-gray-800">
 										Billing Address
 									</p>
 									<p class="w-48 lg:w-full dark:text-gray-300 xl:w-48 text-center md:text-left text-sm leading-5 text-gray-600">
 										180 North King Street, Northhampton MA 1060
 									</p>
-								</div>
+								</div> */}
 							</div>
 							{/* <div class="flex w-full justify-center items-center md:justify-start md:items-start">
 								<button class="mt-6 md:mt-0 dark:border-white dark:hover:bg-gray-900 dark:bg-transparent dark:text-white py-5 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 border border-gray-800 font-medium w-96 2xl:w-full text-base font-medium leading-4 text-gray-800">
